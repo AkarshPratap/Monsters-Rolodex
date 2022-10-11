68e4ddc23aaa6258b24a7e0650b49ed9c710c383
unknown <akarshpratap3107@gmail.com>
2022-10-11 22:38:12 +0530

    class components used

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import Card from '../card/card.component'
+import './card-list.styles.css';
+
+class CardList extends Component{
+    render() {
+        const { monsters } = this.props;
+        return(
+            <div className='card-list'> 
+                {monsters.map((monster, index) => <Card key={index} monster={monster}/>)}            
+            </div>
+        );
+    }
+}
+
+export default CardList;
